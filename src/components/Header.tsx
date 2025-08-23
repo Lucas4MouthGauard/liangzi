408b09997e06e0682698892f8c1b02a15f1d2cf4
Lucas4MouthGauard <biaoli888@outlook.com>
2025-08-24 01:29:10 +0800

    更新推特链接为 https://x.com/perpcat_sol

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,7 +39,7 @@ const Header: React.FC = () => {
             <li><button onClick={() => scrollToSection('interactive-demo')}>Interactive Demo</button></li>
             <li>
               <a 
-                href="https://x.com/PerpCat_pump" 
+                href="https://x.com/perpcat_sol" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="nav-x-button"
